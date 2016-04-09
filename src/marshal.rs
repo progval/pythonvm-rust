aeb902e0d5111d3355f0e2fd5b7ff4958c0dc377
Valentin Lorentz <progval+git@progval.net>
2016-04-10 01:24:45 +0200

    Add support for unmarshalling circular references.

--- src/marshal.rs
+++ src/marshal.rs
@@ -24,6 +24,7 @@ impl fmt::Display for UnmarshalError {
 #[derive(PartialEq)]
 #[derive(Debug)]
 pub enum Object {
+    Hole, // Temporary object for unmarshalling
     //Null,
     None,
     False,
@@ -38,14 +39,14 @@ pub enum Object {
     String(::std::string::String),
     //Interned,
     //Ref_,
-    Tuple(Vec<Arc<Object>>),
-    //List,
+    Tuple(Vec<Object>),
+    List(Vec<Object>),
     //Dict,
     Code(Arc<Object>),
     //Unknown,
     //Set,
     //FrozenSet,
-    //Ref,
+    Ref(u32),
 
     Bytes(Vec<u8>), // aka. ASCII in CPython's marshal
     //ShortAscii,
@@ -104,32 +105,35 @@ fn read_unicode_string<R: io::Read>(r: &mut R, size: usize) -> Result<String, Un
     }
 }
 
-fn read_tuple<R: io::Read>(r: &mut R, references: &mut Vec<Arc<Object>>, size: usize) -> Result<Vec<Arc<Object>>, UnmarshalError> {
-    let mut vector = Vec::<Arc<Object>>::new();
+fn read_objects<R: io::Read>(r: &mut R, references: Vec<Object>, size: usize) -> Result<(Vec<Object>, Vec<Object>), UnmarshalError> {
+    let mut vector = Vec::<Object>::new();
+    let mut references2 = references;
     vector.reserve(size);
     for _ in 0..size {
-        vector.push(try!(read_object(r, references)))
+        let (object, r) = try!(read_object(r, references2));
+        references2 = r;
+        vector.push(object);
     };
-    Ok(vector)
+    Ok((vector, references2))
 }
 
-pub fn read_object<R: io::Read>(r: &mut R, references: &mut Vec<Arc<Object>>) -> Result<Arc<Object>, UnmarshalError> {
+pub fn read_object<R: io::Read>(r: &mut R, mut references: Vec<Object>) -> Result<(Object, Vec<Object>), UnmarshalError> {
     let byte = read_byte!(r);
     let flag = byte & 0b10000000;
     let opcode = byte & 0b01111111;
-    let (add_ref, object_arc) = match opcode as char {
+    let (add_ref, object, mut references) = match opcode as char {
         '0' => return Err(UnmarshalError::UnexpectedCode("NULL object in marshal data for object".to_string())),
-        'N' => (false, Arc::new(Object::None)),
-        'F' => (false, Arc::new(Object::False)),
-        'T' => (false, Arc::new(Object::True)),
-        'i' => (true, Arc::new(Object::Int(try!(read_long(r))))),
+        'N' => (false, Object::None, references),
+        'F' => (false, Object::False, references),
+        'T' => (false, Object::True, references),
+        'i' => (true, Object::Int(try!(read_long(r))), references),
         'z' | 'Z' => { // “short ascii”, “short ascii interned”
             let size = read_byte!(r) as usize;
-            (true, Arc::new(Object::String(try!(read_ascii_string(r, size)))))
+            (true, Object::String(try!(read_ascii_string(r, size))), references)
         },
         'u' => { // “unicode”
             let size = try!(read_long(r)) as usize; // TODO: overflow check if usize is smaller than u32
-            (true, Arc::new(Object::String(try!(read_unicode_string(r, size)))))
+            (true, Object::String(try!(read_unicode_string(r, size))), references)
         }
         's' => { // “string”, but actually bytes
             let size = try!(read_long(r)) as usize; // TODO: overflow check if usize is smaller than u32
@@ -139,36 +143,54 @@ pub fn read_object<R: io::Read>(r: &mut R, references: &mut Vec<Arc<Object>>) ->
                 Err(err) => return Err(UnmarshalError::Io(err)),
                 Ok(()) => ()
             };
-            (true, Arc::new(Object::Bytes(buf)))
+            (true, Object::Bytes(buf), references)
         },
         ')' => { // “small tuple”
             let size = read_byte!(r) as usize;
-            (true, Arc::new(Object::Tuple(try!(read_tuple(r, references, size)))))
+            let (objects, references) = try!(read_objects(r, references, size));
+            (true, Object::Tuple(objects), references)
         },
         '(' => { // “tuple”
             let size = try!(read_long(r)) as usize; // TODO: overflow check if usize is smaller than u32
-            (true, Arc::new(Object::Tuple(try!(read_tuple(r, references, size)))))
+            let index = references.len() as u32; // TODO: overflow check
+            references.push(Object::Hole);
+            let (objects, mut references) = try!(read_objects(r, references, size));
+            references[index as usize] = Object::Tuple(objects); // TODO: overflow check
+            (false, Object::Ref(index), references)
         },
+        '[' => { // “list”
+            let size = try!(read_long(r)) as usize; // TODO: overflow check if usize is smaller than u32
+            let (objects, references) = try!(read_objects(r, references, size));
+            (true, Object::List(objects), references)
+        }
         'r' => {
-            let index = try!(read_long(r)) as usize; // TODO: overflow check if usize is smaller than u32
-            (false, try!(references.get(index).ok_or(UnmarshalError::InvalidReference)).clone())
+            let index = try!(read_long(r));
+            (false, Object::Ref(index), references)
         },
 
         _ => panic!(format!("Unsupported opcode: {}", opcode as char)),
     };
     if flag == 0 || !add_ref {
-        Ok(object_arc)
+        Ok((object, references))
     } else {
-        references.push(object_arc.clone());
-        Ok(object_arc)
+        let index = references.len() as u32; // TODO: overflow check
+        references.push(object);
+        Ok((Object::Ref(index), references))
     }
 }
 
 macro_rules! assert_unmarshal {
-    ( $obj:expr, $bytecode:expr) => {{
+    ( $expected_obj:expr, $bytecode:expr) => {{
         let mut reader: &[u8] = $bytecode;
-        assert_eq!(Arc::new($obj), read_object(&mut reader, &mut Vec::new()).unwrap());
-    }}
+        let (obj, _refs) = read_object(&mut reader, Vec::new()).unwrap();
+        assert_eq!($expected_obj, obj);
+    }};
+    ( $expected_obj:expr, $expected_refs:expr, $bytecode:expr) => {{
+        let mut reader: &[u8] = $bytecode;
+        let (obj, refs) = read_object(&mut reader, Vec::new()).unwrap();
+        assert_eq!($expected_obj, obj);
+        assert_eq!($expected_refs, refs);
+    }};
 }
 
 #[test]
@@ -182,30 +204,36 @@ fn test_basics() {
 
 #[test]
 fn test_int() {
-    assert_unmarshal!(Object::Int(0), b"\xe9\x00\x00\x00\x00");
+    assert_unmarshal!(Object::Ref(0), vec![Object::Int(0)], b"\xe9\x00\x00\x00\x00");
 
-    assert_unmarshal!(Object::Int(5), b"\xe9\x05\x00\x00\x00");
+    assert_unmarshal!(Object::Ref(0), vec![Object::Int(5)], b"\xe9\x05\x00\x00\x00");
 
-    assert_unmarshal!(Object::Int(1000), b"\xe9\xe8\x03\x00\x00");
+    assert_unmarshal!(Object::Ref(0), vec![Object::Int(1000)], b"\xe9\xe8\x03\x00\x00");
 }
 
 #[test]
 fn test_string() {
-    assert_unmarshal!(Object::String("foo".to_string()), b"\xda\x03foo");
+    assert_unmarshal!(Object::Ref(0), vec![Object::String("foo".to_string())], b"\xda\x03foo");
 
     // Note: this string was not generated with the marshal module
-    assert_unmarshal!(Object::String("fooé".to_string()), b"\xda\x04foo\xe9");
+    assert_unmarshal!(Object::Ref(0), vec![Object::String("fooé".to_string())], b"\xda\x04foo\xe9");
 
-    assert_unmarshal!(Object::String("fooé".to_string()), b"\xf5\x05\x00\x00\x00foo\xc3\xa9");
+    assert_unmarshal!(Object::Ref(0), vec![Object::String("fooé".to_string())], b"\xf5\x05\x00\x00\x00foo\xc3\xa9");
 }
 
 #[test]
 fn test_bytes() {
-    assert_unmarshal!(Object::Bytes(vec!['f' as u8, 'o' as u8, 5]), b"\xf3\x03\x00\x00\x00fo\x05");
+    assert_unmarshal!(Object::Ref(0), vec![Object::Bytes(vec!['f' as u8, 'o' as u8, 5])], b"\xf3\x03\x00\x00\x00fo\x05");
 }
 
 #[test]
 fn test_references() {
-    let item = Arc::new(Object::String("foo".to_string()));
-    assert_unmarshal!(Object::Tuple(vec![item.clone(), item.clone()]), b")\x02\xda\x03foor\x00\x00\x00\x00")
+    assert_unmarshal!(Object::Tuple(vec![Object::Ref(0), Object::Ref(0)]), b")\x02\xda\x03foor\x00\x00\x00\x00")
+}
+
+#[test]
+fn test_recursive_reference() {
+    // l = []; l.append(l)
+    assert_unmarshal!(Object::Ref(0), vec![Object::List(vec![Object::Ref(0)])], b"\xdb\x01\x00\x00\x00r\x00\x00\x00\x00'");
+
 }
